00058a524be56ddd1574fd4713b0ff69021b1faa
Basya <Basya@volodymyrs-mbp>
2023-10-31 18:45:51 +0200

    unidirectional association

--- src/main/java/telran/java48/book/dao/PublisherRepository.java
+++ src/main/java/telran/java48/book/dao/PublisherRepository.java
@@ -1,5 +1,7 @@
 package telran.java48.book.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,8 +11,11 @@ public interface PublisherRepository extends JpaRepository<Publisher, String> {
 
 	//@Query(value = "SELECT b.publisher_publisher_name FROM BOOK b JOIN BOOK_AUTHORS ba ON b.isbn = ba.book_isbn WHERE ba.author_name = 'Author2'"
 	//		, nativeQuery = true)
-	@Query("select b.publisher.publisherName from Book b join Book_Authors ba on b.isbn = ba.book.isbn where ba.author.name = ?1")
-	Iterable<String> findPublishersByAuthor(String authorName);
+//	@Query("select b.publisher.publisherName from Book b join Book_Authors ba on b.isbn = ba.book.isbn where ba.author.name = ?1")
+//	List<String> findPublishersByAuthor(String authorName);
+	
+	@Query("select distinct p.publisherName from Book b join b.authors a join b.publisher p where a.name =?1")
+	List<String> findPublishersByAuthor(String authorName);
 }
 
 
